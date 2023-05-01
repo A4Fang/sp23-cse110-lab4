58a884e2d8fa7a72001260f658a2bb542b253336
A4Fang <117263182+A4Fang@users.noreply.github.com>
2023-04-30 17:17:11 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,11 +1,11 @@
-let statistics =  {
+/* let statistics =  {
     redCars: 21,
     blueCars: 45,
     greenCars: 12,
     raceCars: 5,
     blackCars: 40,
     rareCars: 2
-};
+}; */
 
 for(const car in statistics) {
     if(statistics[car] % 2 == 1 || car.charAt(0) == 'r') {
